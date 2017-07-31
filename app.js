b2b2ceff803b168cf2a356cbcc518262e8c81fba
David Berger <dgaberger@gmail.com>
2017-07-31 18:34:26 -0400

    up to nunjucks

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const volleyball = require("volleyball")
+const nunjucks = require("nunjucks")
 
 //logger
 app.use(volleyball)
@@ -17,4 +18,24 @@ app.get("/news", function(req, res){
 app.get("/", function(req, res){
 	res.send("Welcome Buddy")
 })
+
+
+
+//nunjucks
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+//nunjucks.configure('views', {noCache: true});
+// var renderHTML = nunjucks.render("index.html", locals, function(err, output){
+// 	console.log(output)
+// })
+// app.set('view engine', 'html'); // have res.render work with html files
+// app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+// nunjucks.configure('views'); // point nunjucks to the proper directory for templates
 
